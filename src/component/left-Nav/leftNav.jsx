979efed16449982699b8529ec8a8a07c57845791
1198994985 <1198994985@qq.com>
2019-08-28 17:56:04 +0800

    首页框架hooks重写

--- src/component/left-Nav/leftNav.jsx
+++ src/component/left-Nav/leftNav.jsx
@@ -1,5 +1,5 @@
 
-import React, { Component } from 'react'
+import React, { Component, useState, useEffect } from 'react'
 import logo from './min_logo.png'
 import { Layout, Menu, Icon } from 'antd';
 import './index.less'
@@ -9,24 +9,24 @@ import menuList from '../../config/menuConfig'
 const { Sider } = Layout;
 const { SubMenu } = Menu;
 
-class LeftNav extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      collapsed: false,
-      onbutton: true
-    };
-    this.menuNodes = this.getMenuNodes(menuList)    
-  }
+
+
+function LeftNav(props) {
+  let openKey; //决定左侧导航选中元素
+  let path = props.location.pathname; // 获取当前页面地址
+  const [collapsed,setCollapsed] = useState(false);
+  const [list,serList] = useState([]);
+  // const [onbutton,setOnButton] = useState(true);
+
 
   /*
   根据menu的数据数组生成对应的标签数组
   map() + 递归调用
   */
-  getMenuNodes = (menuList) => {
-    //获取当前路径
-    const path = this.props.location.pathname;
-    console.log("风波最帅2")
+  const getMenuNodes = (menuList) => {
+    console.log('getMenuNodes')
+
+    // 一级菜单
     return menuList.map( (item) => {
       if(!item.children) {
         return (
@@ -41,58 +41,56 @@ class LeftNav extends Component {
         //菜单选中
         const cItem = item.children.find( cItem => cItem.key === path );
         if(cItem) {
-          this.openKey = item.key
+          openKey = item.key
         }
 
         return (
           <SubMenu
-          key={item.key}
-          title={
-            <span>
-              <Icon type={item.icon} />
-              <span>{"  " + item.title}</span>
-            </span>
-          }
-        >
-          {item.children.map( (item) => {
-            return (
-              <Menu.Item key={item.key}>
-                <Link to={item.key}>
-                  <Icon type={item.icon} />
-                  <span> {item.title} </span>
-                </Link>
-              </Menu.Item>
-            )
-          } )}
+            key={item.key}
+            title={
+              <span>
+                <Icon type={item.icon} />
+                <span>{"  " + item.title}</span>
+              </span>
+            }
+          >
 
-        </SubMenu>
+            {/* // 二级菜单 */}
+            {item.children.map( (item) => {
+              return (
+                <Menu.Item key={item.key}>
+                  <Link to={item.key}>
+                    <Icon type={item.icon} />
+                    <span> {item.title} </span>
+                  </Link>
+                </Menu.Item>
+              )
+            } )}
+          </SubMenu>
         )
+
       }
     } )
   }
 
-  onCollapse = collapsed => {
-    this.setState({ collapsed });
+
+  useEffect( ()=> {
+    // 得到需要打开菜单项的key
+    serList(getMenuNodes(menuList));
+  },[] )
+
+  const onCollapse = collpd => {
+    setCollapsed(collpd);
+    console.log('onCollapse')
     // if(collapsed === false) {
-    //   setTimeout(()=>(this.setState((preState) => {
-    //     return { onbutton:!preState.onbutton}
-    //   })),150)
+    //   setTimeout(() => (setOnButton(!onbutton)),150)
     //   console.log('展开')
     // } else {
-    //   this.setState((preState) => {
-    //     return { onbutton:!preState.onbutton}
-    //   })
+    //    setOnButton(!onbutton))
     // }
   };
-
-  render() {
-
-    const path = this.props.location.pathname
-    // 得到需要打开菜单项的key
-    const openKey = this.openKey
-
     return (
-      <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse} >
+      <Sider collapsible collapsed={collapsed} onCollapse={onCollapse} >
         <div className="left-nav">
             <Link to='/home/' className="left-nav-header">
               <img className="left-nav-logo" src={logo} alt="logo"/>
@@ -100,11 +98,11 @@ class LeftNav extends Component {
         </div>
 
         <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline"  selectedKeys={[path]} defaultOpenKeys={[openKey]} >
-          { this.menuNodes }
+          { list }
         </Menu>
     </Sider>
     )
   }
-}
+
 
 export default withRouter(LeftNav);
